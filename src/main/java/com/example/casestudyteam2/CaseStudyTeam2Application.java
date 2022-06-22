46d0bea6ecf7672f2d755618b501b8d96fe2d52a
unknown <hung.pnpt93@gmail.com>
2022-06-22 10:33:33 +0700

    l1

---
+++ src/main/java/com/example/casestudyteam2/CaseStudyTeam2Application.java
@@ -0,0 +1,13 @@
+package com.example.casestudyteam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyTeam2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyTeam2Application.class, args);
+    }
+
+}
