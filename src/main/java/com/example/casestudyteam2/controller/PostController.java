81d0117ea0f4661a570cb4529b2f27e79bf594e6
unknown <qquangmminh19999@gmail.com>
2022-06-24 22:54:15 +0700

    aaaaabbba

--- src/main/java/com/example/casestudyteam2/controller/PostController.java
+++ src/main/java/com/example/casestudyteam2/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.casestudyteam2.controller;
 
 import com.example.casestudyteam2.model.Post;
 import com.example.casestudyteam2.service.IPostService;
+import com.example.casestudyteam2.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
